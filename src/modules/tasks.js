2be69777b4f0cede79c227812ad40c3924c60457
wise-guru <wise.guru123@gmail.com>
2022-07-08 16:26:18 -0400

    Add validate form

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -3,17 +3,26 @@ import Delete from '../img/delete.png'
 
 let taskDatabase = []
 
-function Task(title, description, date, priority, categories, close) {
+function Task(title, description, date, priority, categories) {
     this.title = title;
     this.description = description;
     this.date = date;
     this.priority = priority;
     this.categories = categories;
-    this.close = close;
 }
 
-function addTask(title, description, date, priority, categories, close) {
-    let task = new Task(title, description, date, priority, categories, close)
+function validateForm(e) {
+    e.preventDefault()
+
+    if (title.value !== '') {
+        addTask(title.value, description.value, date.value, priority.value, categories.value)
+    }
+
+    document.querySelector('.task-form').reset()
+}
+
+function addTask(title, description, date, priority, categories) {
+    let task = new Task(title, description, date, priority, categories)
     taskDatabase.push(task);
     showTaskinfo(task);
 }
@@ -28,36 +37,43 @@ function showTaskinfo(task) {
     taskRow.classList.add('.task-row')
     taskContainer.appendChild(taskRow)
 
-    const taskTitle = document.createElement('div')
-    taskTitle.classList.add('task-left')
-    taskRow.appendChild(taskTitle)
 
-        const taskLabel = document.createElement('label')
-        taskLabel.htmlFor = 'done'
+        const firstRow = document.createElement('div')
+        taskRow.appendChild(firstRow)
 
-        const doneTask = document.createElement('input')
-        doneTask.type = 'checkbox'
-        doneTask.name = 'done'
-        doneTask.id = 'done'
-    
-    const taskIcons = document.createElement('div')
-    taskIcons.classList.add('task-right')
-    taskRow.appendChild(taskIcons)
+            const taskTitle = document.createElement('div')
+            taskTitle.classList.add('task-left')
+            firstRow.appendChild(taskTitle)
 
-        const taskEdit = new Image()
-        taskEdit.src = Edit;
-        taskIcons.appendChild(taskEdit)
+                const taskLabel = document.createElement('label')
+                taskLabel.htmlFor = 'done'
+                taskLabel.textContent = task.label;
 
-        const taskDelete = new Image()
-        taskDelete.src = Delete;
-        taskIcons.appendChild(taskDelete)
+                const doneTask = document.createElement('input')
+                doneTask.type = 'checkbox'
+                doneTask.name = 'done'
+                doneTask.id = 'done'
+        
+            const taskIcons = document.createElement('div')
+            taskIcons.classList.add('task-right')
+            firstRow.appendChild(taskIcons)
 
+                const taskEdit = new Image()
+                taskEdit.src = Edit;
+                taskIcons.appendChild(taskEdit)
 
+                const taskDelete = new Image()
+                taskDelete.src = Delete;
+                taskIcons.appendChild(taskDelete)
+        
+        const secondRow = document.createElement('div')
+        taskRow.appendChild(secondRow)
 
+            const taskDescription = document.createElement('div')
+            taskDescription.textContent = `Description: ${task.description}`;
 
-
-    
-
-    
-
+            const taskDate = document.createElement('div')
+            taskDate.textContent = `Due date: ${task.date}`
 }
+
+export default validateForm
