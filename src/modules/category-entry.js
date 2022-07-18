76ea218d88a0022077177c0db7c2ef8599c380c1
wise-guru <wise.guru123@gmail.com>
2022-07-17 23:05:58 -0400

    Add categories to array

---
+++ src/modules/category-entry.js
@@ -0,0 +1,32 @@
+export default class Category {
+    constructor (title) {
+        this.title = title;
+        this.tasks = []
+    }
+
+    // getTitle() {
+    //     return this.title;
+    // }
+
+    // setTitle(newTitle) {
+    //     this.title = newTitle;
+    // }
+
+    // addTasktoCategory(task) {
+    //     this.tasks.push(task);
+    // }
+
+    // getTaskfromCategory(task) {
+    //     return this.tasks.find(title => title.title === task);
+    // }
+
+    // getTasks() {
+    //     return this.tasks;
+    // }
+
+    // resetTasks() {
+    //     this.tasks = []
+    // }
+
+}
+
