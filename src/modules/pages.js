8ed989cdc8ee9b5b6a778de115cdd3921839e595
wise-guru <wise.guru123@gmail.com>
2022-07-21 12:34:07 -0400

    Change name of file

---
+++ src/modules/pages.js
@@ -0,0 +1,82 @@
+import { showCategoryPage } from "./categories.js"
+import loadModals from "./modal.js"
+import { showTaskInfo } from "./tasks.js"
+
+function showHome() {
+    const homeContent = document.createElement('div')
+    homeContent.classList.add('home-content')
+
+    const homeTitle = document.createElement('h1')
+    homeTitle.textContent = 'All Tasks'
+    homeContent.appendChild(homeTitle)
+
+    const addTaskBtn = document.createElement('button')
+    addTaskBtn.textContent = "Add Task +"
+    homeContent.appendChild(addTaskBtn)
+
+    addTaskBtn.addEventListener('click', function(e) {
+        loadModals('task')
+    })
+
+
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+    taskContainer.id = 'task-container'
+    homeContent.appendChild(taskContainer)
+
+    return homeContent
+}
+
+function showToday() {
+    const todayContent = document.createElement('div');
+    todayContent.classList.add('today-content');
+
+    const todayTitle = document.createElement('h1');
+    todayTitle.textContent = `Today's Tasks`;
+    todayContent.appendChild(todayTitle);
+
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+    taskContainer.id = 'task-container'
+    todayContent.appendChild(taskContainer)
+
+    return todayContent
+}
+
+function showThisWeek() {
+    const weekContent = document.createElement('div')
+    weekContent.classList.add('week-content');
+
+    const weekTitle = document.createElement('h1');
+    weekTitle.textContent = `This Week's Tasks`;
+    weekContent.appendChild(weekTitle);
+
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+    taskContainer.id = 'task-container'
+    weekContent.appendChild(taskContainer)
+
+    return weekContent
+}
+
+
+
+
+
+function loadPage(e) {
+    const main = document.querySelector('#main');
+    main.textContent = '';
+
+    if (e === 'home') {
+        main.appendChild(showHome());
+        showTaskInfo
+    }   else if (e === 'today') {
+        main.appendChild(showToday());
+    }   else if (e === 'week') {
+        main.appendChild(showThisWeek());
+    }   else {
+        main.appendChild(showCategoryPage(e))
+    }
+}
+
+export {loadPage}
