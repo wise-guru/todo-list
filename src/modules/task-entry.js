37730a52aa7f6431ae46ae09c4c8e5dc998ea93b
wise-guru <wise.guru123@gmail.com>
2022-07-21 12:32:32 -0400

    Revert changes

--- src/modules/task-entry.js
+++ src/modules/task-entry.js
@@ -1,11 +1,13 @@
 export default class Task {
 
-    constructor (title, description, date, priority, categories) {
+    constructor (title, description, date, priority, categories, id, isCompleted) {
         this.title = title;
         this.description = description;
         this.date = date;
         this.priority = priority;
         this.categories = categories;
+        this.id = id;
+        this.isCompleted = isCompleted;
     }
 }
 
