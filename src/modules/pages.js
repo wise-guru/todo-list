31cdd490f4c0d3557393ed004e268767348f7ee0
wise-guru <wise.guru123@gmail.com>
2022-07-24 20:35:04 -0400

    Add classes for styling

--- src/modules/pages.js
+++ src/modules/pages.js
@@ -5,13 +5,14 @@ import { showTaskInfo } from "./tasks.js"
 
 function showAllTasks() {
     const allTasksContent = document.createElement('div')
-    allTasksContent.classList.add('home-content')
+    allTasksContent.classList.add('home-content', 'page')
 
     const allTasksTitle = document.createElement('h1')
     allTasksTitle.textContent = 'All Tasks'
     allTasksContent.appendChild(allTasksTitle)
 
     const addTaskBtn = document.createElement('button')
+    addTaskBtn.classList.add('addTask-btn')
     addTaskBtn.textContent = "Add Task +"
     allTasksContent.appendChild(addTaskBtn)
 
@@ -29,7 +30,7 @@ function showAllTasks() {
 
 function showHome() {
     const homeContent = document.createElement('div')
-    homeContent.classList.add('home-content')
+    homeContent.classList.add('home-content', 'page')
 
     const homeTitle = document.createElement('h1')
     homeTitle.textContent = 'Home'
@@ -38,6 +39,7 @@ function showHome() {
     const addTaskBtn = document.createElement('button')
     addTaskBtn.textContent = "Add Task +"
     homeContent.appendChild(addTaskBtn)
+    addTaskBtn.classList.add('addTask-btn')
 
     addTaskBtn.addEventListener('click', function(e) {
         loadModals('task')
@@ -54,11 +56,20 @@ function showHome() {
 
 function showToday() {
     const todayContent = document.createElement('div');
-    todayContent.classList.add('today-content');
+    todayContent.classList.add('today-content', 'page');
 
     const todayTitle = document.createElement('h1');
     todayTitle.textContent = `Today's Tasks`;
     todayContent.appendChild(todayTitle);
+
+    const addTaskBtn = document.createElement('button')
+    addTaskBtn.textContent = "Add Task +"
+    todayContent.appendChild(addTaskBtn)
+    addTaskBtn.classList.add('addTask-btn')
+
+    addTaskBtn.addEventListener('click', function(e) {
+        loadModals('task')
+    })
 
     const taskContainer = document.createElement('div')
     taskContainer.classList.add('task-container')
@@ -70,11 +81,20 @@ function showToday() {
 
 function showThisWeek() {
     const weekContent = document.createElement('div')
-    weekContent.classList.add('week-content');
+    weekContent.classList.add('week-content', 'page');
 
     const weekTitle = document.createElement('h1');
     weekTitle.textContent = `This Week's Tasks`;
     weekContent.appendChild(weekTitle);
+
+    const addTaskBtn = document.createElement('button')
+    addTaskBtn.textContent = "Add Task +"
+    weekContent.appendChild(addTaskBtn)
+    addTaskBtn.classList.add('addTask-btn')
+
+    addTaskBtn.addEventListener('click', function(e) {
+        loadModals('task')
+    })
 
     const taskContainer = document.createElement('div')
     taskContainer.classList.add('task-container')
@@ -84,6 +104,22 @@ function showThisWeek() {
     return weekContent
 }
 
+function showCompletedTasks() {
+    const completedContent = document.createElement('div')
+    completedContent.classList.add('completed-content', 'page')
+
+    const completedTitle = document.createElement('h1')
+    completedTitle.textContent = 'Completed Tasks'
+    completedContent.appendChild(completedTitle);
+    
+
+    const taskContainer = document.createElement('div')
+    taskContainer.classList.add('task-container')
+    taskContainer.id = 'task-container'
+    completedContent.appendChild(taskContainer)
+
+    return completedContent
+}
 
 function loadPage(e) {
     const main = document.querySelector('#main');
@@ -97,7 +133,11 @@ function loadPage(e) {
         main.appendChild(showToday());
     }   else if (e === 'week') {
         main.appendChild(showThisWeek());
-    }   else {
+    }   else if(e === 'completed') {
+        main.appendChild(showCompletedTasks())
+    }
+    
+    else {
         main.appendChild(showCategoryPage(e))
     }
 }
